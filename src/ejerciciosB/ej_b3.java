390ae644be3cdce2ca2184714ac38ae9b0a5419c
mvcasillas <mvcasillasdiaz@gmail.com>
2022-04-19 17:55:03 +0200

    ej b3 y ej b4 hechos

--- src/ejerciciosB/ej_b3.java
+++ src/ejerciciosB/ej_b3.java
@@ -5,6 +5,9 @@
 package ejerciciosB;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 /**
@@ -16,17 +19,54 @@ public class ej_b3 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         
-        try{
-            File aLeer = new File("");
-            File aEscribir = new File("");
+        File aLeer;
+        File aEscribir;
         
+        do{
+            System.out.println("Introduce el archivo a leer");
+            String rutaLeer = teclado.nextLine();       
+            aLeer = new File(rutaLeer);
+        
+            if(!aLeer.exists()){
+                System.out.println("El archivo que has introducido no existe");
+            }
+            
+        }while(!aLeer.exists());
+        
+        System.out.println("Introduce el archivo a escribir");
+        String rutaEscribir = teclado.nextLine();
+        aEscribir = new File(rutaEscribir);
+        
+        
+        try{
             Scanner lector = new Scanner(aLeer);
+            FileWriter escritor= new FileWriter(aEscribir);
+            
+            ArrayList<String> personas = new ArrayList();
+            
+            while(lector.hasNext()){
+                personas.add(lector.nextLine());
+            }
+            
+            Collections.sort(personas);
+            
+            for(String s:personas){
+                //System.out.println(s.toString());
+                escritor.write(s.toString());
+                escritor.write("\n");
+            }    
+            
+            lector.close();
+            escritor.close();
+            
         }catch(Exception e){
             System.err.println("La has cagao");
         }
         
+        System.out.println("¡Hecho!");
         
-        
+               
     }
+
     
 }
